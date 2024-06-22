721d91908cf68812578ed97e004c38685800d124
sadegh <sadegh15khedry@gmail.com>
2024-06-22 17:04:26 +0330

    added the data preprocessing notebook

--- src/data_prepocessing.py
+++ src/data_prepocessing.py
@@ -5,11 +5,11 @@ def load_data(file_name):
     return pd.read_csv('../datasets/'+file_name)
 
 def split_data(df, feature_column, label_column, test_size=.2, random_state=50):
-    x = df.drop(columns= [feature_column])
-    y = df.drop(columns= [label_column])
-    return train_test_split(x, y, test_size=test_size, random_state=random_state)
+    # x = df.drop(columns= [feature_column])
+    # y = df.drop(columns= [label_column])
+    return train_test_split(df[feature_column], df[label_column], test_size=test_size, random_state=random_state)
 
 def preprocess_data(df):
     df = df.dropna()
-    df = df.dropd_duplicates(keep=False)
+    df.drop_duplicates(keep=False) 
     return df    
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import csv
 
 def save_confution_matrix(cm, file_path):
     plt.figure(figsize=(10, 7))
-    sns.heatmap(cm, annot=True, fmt='d' cmap='blue')
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
     plt.ylabel("Actual")
     plt.xlabel("Predicted")
     plt.savefig(file_path)
@@ -13,3 +14,7 @@ def save_confution_matrix(cm, file_path):
 def save_report(report, file_path):
     with open(file_path, 'w') as f:
         f.write(report)
+        
+        
+def save_dataframe_as_csv(df, file_path):
+    df.to_csv(file_path, index=False)
